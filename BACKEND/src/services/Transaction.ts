b7f3d8d6ced3b5a4799295e072d0de8dea63f2af
Manjeet Roy <ymanjeet7063@gmail.com>
2025-03-08 18:08:55 +0530

    transaction services

---
+++ BACKEND/src/services/Transaction.ts
@@ -0,0 +1,63 @@
+import { ITransaction, Transaction } from "src/models/Transaction";
+
+export class transactionService {
+  async createTransaction(data: ITransaction) {
+    try {
+      const newTransaction = await Transaction.create(data);
+      return newTransaction;
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  }
+
+  async getTransactions() {
+    try {
+      const transactions = await Transaction.find();
+      return transactions;
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  }
+
+  async getTransaction(id: string) {
+    try {
+      const transaction = await Transaction.findById({ _id: id });
+      if (!transaction) {
+        return "Transaction not available";
+      }
+      return transaction;
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  }
+
+  //update a transaction
+  async updateTransaction(id: string, data: ITransaction) {
+    try {
+      const transactions = await Transaction.findByIdAndUpdate(
+        { _id: id },
+        data,
+        { new: true }
+      );
+      if (!transactions) {
+        return "Transaction not available";
+      }
+      return transactions;
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  }
+
+  //delete a transaction
+  async deleteTransaction(id: string) {
+    try {
+      const transaction = await Transaction.findByIdAndDelete(id);
+      if (!transaction) {
+        return "Transaction not available";
+      }
+      console.log("Deleted transaction is: ", transaction);
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  }
+}
